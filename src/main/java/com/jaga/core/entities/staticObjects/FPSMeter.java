afbb2022e354b0970f3957bf2edf6a7427be4a6b
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-06-01 17:12:14 +0200

    create FPS counter

---
+++ src/main/java/com/jaga/core/entities/staticObjects/FPSMeter.java
@@ -0,0 +1,32 @@
+package com.jaga.core.entities.staticObjects;
+
+import java.awt.*;
+
+public class FPSMeter extends StaticEntity{
+    private int frameCount;
+    private long lastTime;
+    double fps;
+
+    public FPSMeter(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        frameCount = 0;
+        lastTime = System.currentTimeMillis();
+    }
+
+
+    public void update() {
+        frameCount++;
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastTime >= 1000) {
+            fps = (double) frameCount / (currentTime - lastTime) * 1000;
+            frameCount = 0;
+            lastTime = currentTime;
+        }
+    }
+
+    public void draw(Graphics g) {
+        g.drawString("FPS: " + String.format("%.2f", fps), x, y);
+    }
+
+
+}
