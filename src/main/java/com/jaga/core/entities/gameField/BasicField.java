5c4a6f593ee2093b178535ccfc929c38dc053c30
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-06-01 18:03:56 +0200

    rewrite code and add side walls

--- src/main/java/com/jaga/core/entities/gameField/BasicField.java
+++ src/main/java/com/jaga/core/entities/gameField/BasicField.java
@@ -26,39 +26,22 @@ public class BasicField {
         int backgroundWidth = (int) (screenWidth * 0.15);
 
         List<Wall> walls = new ArrayList<>();
+        walls.addAll(createMainWallFrame(backgroundWidth, screenWidth, screenHeight));
+        walls.addAll(createSideLinesGround(backgroundWidth, screenWidth, screenHeight));
+
+
+        return walls;
+    }
+
+    private List<Wall> createMainWallFrame(int backgroundWidth, int screenWidth, int screenHeight){
+        List<Wall> walls = new ArrayList<>();
         int heightCountWalls = (screenHeight / ConfigEntity.wallHeight);
         int widthCountWalls = ((screenWidth - (backgroundWidth *2 )) / ConfigEntity.wallWidth) + 1;
 
-        System.out.println("Width: " + screenWidth + " Height: " + screenHeight);
-        System.out.println("WidthCount: " + widthCountWalls + " HeightCount: " + heightCountWalls);
-
-
         Wall wallLeft = new Wall(backgroundWidth, ConfigEntity.wallHeight, ConfigEntity.wallWidth, screenHeight - ConfigEntity.wallHeight * 2);
-        Wall wallRight = new Wall(screenWidth - backgroundWidth, 0, ConfigEntity.wallWidth, screenHeight - ConfigEntity.wallHeight );
+        Wall wallRight = new Wall(screenWidth - backgroundWidth - ConfigEntity.wallWidth, 0, ConfigEntity.wallWidth, screenHeight - ConfigEntity.wallHeight);
         Wall wallTop = new Wall(backgroundWidth, 0,  (screenWidth - backgroundWidth * 2), ConfigEntity.wallHeight);
-        Wall wallBottom = new Wall(backgroundWidth, screenHeight-ConfigEntity.wallHeight,screenWidth - backgroundWidth * 2 + ConfigEntity.wallWidth, ConfigEntity.wallHeight);
-
-
-
-       /* 
-       List<Wall> background = new ArrayList<>();
-
-        int innerX = backgroundWidth + ConfigEntity.wallWidth;
-        int innerY = ConfigEntity.wallHeight;
-        int innerWidth = screenWidth - (backgroundWidth * 2) - ConfigEntity.wallWidth * 2;
-        int innerHeight = screenHeight - ConfigEntity.wallHeight * 2;
-
-        Wall innerWall = new Wall(innerX, innerY, innerWidth, innerHeight);
-        innerWall.setTexture(createColorTexture(innerWidth, innerHeight, Color.YELLOW));
-
-        walls.add(innerWall);
-        
-        //я попытался сделать задник но у меня не получилось)
-        // вернее получилось, но из-за этого персонаж не двигается :\
-        // поэтому я закомментировал этот код
-        */
-
-
+        Wall wallBottom = new Wall(backgroundWidth, screenHeight-ConfigEntity.wallHeight,screenWidth - backgroundWidth * 2, ConfigEntity.wallHeight);
 
         walls.add(wallLeft);
         walls.add(wallRight);
@@ -67,15 +50,17 @@ public class BasicField {
         return walls;
     }
 
-    /*
-    private BufferedImage createColorTexture(int width, int height, Color color) {
-        BufferedImage texture = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = texture.createGraphics();
-        g2d.setColor(color);
-        g2d.fillRect(0, 0, width, height);
-        g2d.dispose();
-        return texture;
+    private List<Wall> createSideLinesGround(int backgroundWidth, int screenWidth, int screenHeight){
+        List<Wall> sideLinesGround = new ArrayList<>();
+        int heightCountWalls = (screenHeight / ConfigEntity.wallHeight);
+        int widthCountWalls = (backgroundWidth / ConfigEntity.wallWidth);
+
+        Wall wallLeft = new Wall(0, 0, backgroundWidth, screenHeight, ConfigEntity.sideWallTexture);
+        Wall wallRight = new Wall(screenWidth - backgroundWidth, 0, backgroundWidth, screenHeight,ConfigEntity.sideWallTexture);
+
+        sideLinesGround.add(wallLeft);
+        sideLinesGround.add(wallRight);
+        return sideLinesGround;
     }
-    */
 
 }
