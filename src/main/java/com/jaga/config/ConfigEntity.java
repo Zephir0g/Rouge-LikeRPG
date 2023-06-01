92b6795ec5207d600a03e462f5da9c9ef8f32df9
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-06-01 18:01:24 +0200

    add new variables for texture

--- src/main/java/com/jaga/config/ConfigEntity.java
+++ src/main/java/com/jaga/config/ConfigEntity.java
@@ -4,8 +4,14 @@ public class ConfigEntity {
 
     public static final int playerWidth = 120;
     public static final int playerHeight = 120;
+    public static final String defaultPlayerTexture = "/assets/entity/player.png";
+
 
     public static final int wallWidth = 100;
     public static final int wallHeight = 100;
+    public static final String defaultWallTexture = "/assets/entity/wall.png";
+    public static final String sideWallTexture = "/assets/entity/wall2.jpg";
+
+
 
 }
