fad5607621ae382b1b8bbda6f262d8b1ceb60196
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-06-27 23:55:37 +0200

    add buttons to saves

--- src/main/java/com/jaga/windows/menu/SavesPanel.java
+++ src/main/java/com/jaga/windows/menu/SavesPanel.java
@@ -42,12 +42,109 @@ public class SavesPanel extends JPanel {
         File[] saves = savesFolder.listFiles();
         if (saves.length > 0) {
             for (File save : saves) {
-                JLabel saveLabel = new JLabel(save.getName());
-                savesContentPanel.add(saveLabel);
+                JButton saveButton = new JButton(save.getName());
+                saveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+                saveButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+                saveButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Disable the "Save" button
+                        saveButton.setEnabled(false);
+
+                        // Remove existing buttons
+                        savesContentPanel.removeAll();
+                        savesContentPanel.revalidate();
+                        savesContentPanel.repaint();
+
+                        // Create a new panel for the new buttons
+                        JPanel newButtonsPanel = new JPanel();
+                        newButtonsPanel.setLayout(new BorderLayout());
+
+                        // Set the maximum height of the newButtonsPanel
+                        newButtonsPanel.setMaximumSize(new Dimension(500, 40));
+
+                        // Add the "Save" label at the top
+                        JLabel saveLabel = new JLabel(save.getName());
+                        saveLabel.setHorizontalAlignment(JLabel.CENTER);
+                        newButtonsPanel.add(saveLabel, BorderLayout.NORTH);
+
+                        // Create a panel for the buttons
+                        JPanel buttonsPanel = new JPanel();
+                        buttonsPanel.setLayout(new GridLayout(1, 0, 10, 0)); // Rows, Columns, Horizontal Gap, Vertical Gap
+
+                        // Add new buttons for "Load", "Delete", and "Settings"
+                        JButton loadButton = new JButton("Load");
+                        JButton deleteButton = new JButton("Delete");
+                        JButton settingsButton = new JButton("Settings");
+                        JButton backButton = new JButton("Back");
+
+                        // Add action listeners to the new buttons
+                        loadButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // Perform "Load" action
+                                // Add your implementation here
+                            }
+                        });
+
+                        deleteButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // Perform "Delete" action
+                                // Add your implementation here
+                            }
+                        });
+
+                        settingsButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // Perform "Settings" action
+                                // Add your implementation here
+                            }
+                        });
+
+                        backButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // Re-enable the "Save" button
+                                saveButton.setEnabled(true);
+
+                                // Remove the new buttons panel
+                                savesContentPanel.remove(newButtonsPanel);
+
+                                // Repaint the panel to update the UI
+                                savesContentPanel.revalidate();
+                                savesContentPanel.repaint();
+                                loadSaves();
+                            }
+                        });
+
+                        // Add the new buttons to the buttons panel
+                        buttonsPanel.add(loadButton);
+                        buttonsPanel.add(deleteButton);
+                        buttonsPanel.add(settingsButton);
+                        buttonsPanel.add(backButton);
+
+                        // Add the buttons panel to the new panel
+                        newButtonsPanel.add(buttonsPanel, BorderLayout.CENTER);
+
+                        // Add the new buttons panel to the main panel
+                        savesContentPanel.add(newButtonsPanel);
+
+                        // Repaint the panel to update the UI
+                        savesContentPanel.revalidate();
+                        savesContentPanel.repaint();
+                    }
+                });
+
+                savesContentPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+                savesContentPanel.add(saveButton);
             }
         } else {
             JLabel saveLabel = new JLabel("No saves");
             savesContentPanel.add(saveLabel);
         }
     }
+
 }
